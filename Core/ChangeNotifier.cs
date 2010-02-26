9ddd3ed82ebd83302feabfc4070ec52a53bf2114
alfaproject <alfa.project@gmail.com>
2010-02-26 19:15:03 +0000

    Added helper classes to handle changed property events.

---
+++ Core/ChangeNotifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace Supay.Core {
+  public class ChangeNotifier {
+
+    private Func<PropertyChangedEventHandler> _notifier;
+
+    public ChangeNotifier(Func<PropertyChangedEventHandler> notifier) {
+      _notifier = notifier;
+    }
+
+    public ChangeNotifier<T> Create<T>(Expression<Func<T>> expression) {
+      return new ChangeNotifier<T>(expression, _notifier);
+    }
+
+    public ChangeNotifier<T> Create<T>(Expression<Func<T>> expression, T initialValue) {
+      return new ChangeNotifier<T>(expression, _notifier, initialValue);
+    }
+
+  } //class ChangeNotifier
+} //namespace Supay.Core
