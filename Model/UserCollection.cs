42ef0f477cce220346a2038cf5d8c311536079fe
_aLfa_ <alfa.project@gmail.com>
2010-02-28 16:53:48 +0000

    Cosmetic changes.

--- Model/UserCollection.cs
+++ Model/UserCollection.cs
@@ -1,21 +1,17 @@
 using System;
 using System.Collections.ObjectModel;
-using Supay.Irc.Messages;
 
 namespace Supay.Irc {
 
   /// <summary>
-  ///     <para>
-  ///       A collection that stores <see cref='Supay.Irc.User'/> objects.
-  ///    </para>
-  /// </summary>
+  ///   A collection that stores <see cref="User"/> objects. </summary>
   [Serializable]
   public class UserCollection : ObservableCollection<User> {
 
     /// <summary>
-    /// Removes the first User in the collection which is matched by the Predicate.
-    /// </summary>
-    /// <returns>True if a User was removed, false if no User was removed.</returns>
+    ///   Removes the first User in the collection which is matched by the Predicate. </summary>
+    /// <returns>
+    ///   True if a User was removed, false if no User was removed. </returns>
     public bool RemoveFirst(Predicate<User> match) {
       for (int i = 0; i < this.Count; i++) {
         if (match(this[i])) {
@@ -27,22 +23,14 @@ namespace Supay.Irc {
     }
 
     /// <summary>
-    /// Removes the first User in the collection which has the given nick.
-    /// </summary>
-    public bool RemoveFirst(String nick) {
-      Predicate<User> match = delegate(User userToMatch) {
-        return MessageUtil.IsIgnoreCaseMatch(userToMatch.Nickname, nick);
-      };
-      return this.RemoveFirst(match);
+    ///   Removes the first User in the collection which has the given nick. </summary>
+    public bool RemoveFirst(string nick) {
+      return this.RemoveFirst(u => u.Nickname.Equals(nick, StringComparison.OrdinalIgnoreCase));
     }
 
     /// <summary>
-    /// Finds the first User in the collection which matches the given Predicate.
-    /// </summary>
+    ///   Finds the first User in the collection which matches the given Predicate. </summary>
     public User Find(Predicate<User> match) {
-      if (match == null) {
-        throw new ArgumentNullException("match");
-      }
       for (int i = 0; i < this.Count; i++) {
         if (match(this[i])) {
           return this[i];
@@ -52,54 +40,45 @@ namespace Supay.Irc {
     }
 
     /// <summary>
-    /// Finds the first User in the collection which matches the given nick.
-    /// </summary>
-    public User Find(String nick) {
-      Predicate<User> match = delegate(User userToMatch) {
-        return Supay.Irc.Messages.MessageUtil.IsIgnoreCaseMatch(userToMatch.Nickname, nick);
-      };
-      return Find(match);
+    ///   Finds the first User in the collection which matches the given nick. </summary>
+    public User Find(string nick) {
+      return this.Find(u => u.Nickname.Equals(nick, StringComparison.OrdinalIgnoreCase));
     }
 
     /// <summary>
-    /// Ensures that the collection has a User with the given nick.
-    /// </summary>
+    ///   Ensures that the collection has a User with the given nick. </summary>
     /// <remarks>
-    /// If no User has the given nick, then a new User is created with the nick, and is added to the collection.
-    /// </remarks>
-    /// <param name="nick">The nick to ensure.</param>
-    /// <returns>The User in the collection with the given nick.</returns>
-    public User EnsureUser(String nick) {
-      User knownUser = this.Find(nick);
-      if (knownUser == null) {
-        knownUser = new User(nick);
-        this.Add(knownUser);
+    ///   If no User has the given nick, then a new User is created with the nick, and is added to the collection. </remarks>
+    /// <param name="nick">
+    ///   The nick to ensure. </param>
+    /// <returns>
+    ///   The User in the collection with the given nick. </returns>
+    public User EnsureUser(string nick) {
+      User user = this.Find(nick);
+      if (user == null) {
+        user = new User(nick);
+        this.Add(user);
       }
-      return knownUser;
+      return user;
     }
 
     /// <summary>
-    /// Ensures that the collection has a User which matches the nick of the given User.
-    /// </summary>
+    ///   Ensures that the collection has a User which matches the nick of the given User. </summary>
     /// <remarks>
-    /// If no User matches the given User, then the given User added to the collection.
-    /// If a User is found, then the existing User is merged with the given User.
-    /// </remarks>
-    /// <returns>The User in the collection which matches the given User.</returns>
-    public User EnsureUser(User tempUser) {
-      if (tempUser == null) {
-        throw new ArgumentNullException("tempUser");
-      }
-      User knownUser = this.Find(tempUser.Nickname);
-      if (knownUser == null) {
-        knownUser = tempUser;
-        this.Add(knownUser);
+    ///   If no User matches the given User, then the given User is added to the collection.
+    ///   If a User is found, then the existing User is merged with the given User. </remarks>
+    /// <returns>
+    ///   The User in the collection which matches the given User. </returns>
+    public User EnsureUser(User newUser) {
+      User user = this.Find(newUser.Nickname);
+      if (user == null) {
+        user = newUser;
+        this.Add(user);
       } else {
-        knownUser.CopyFrom(tempUser);
+        user.CopyFrom(newUser);
       }
-      return knownUser;
+      return user;
     }
 
-  }
-
-}
+  } //class UserCollection
+} //namespace Supay.Irc
