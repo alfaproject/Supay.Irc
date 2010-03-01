0467624f16535f3ba2784281d1cee85caf3fd126
_aLfa_ <alfa.project@gmail.com>
2010-03-01 16:47:31 +0000

    Cleaned up IChannelTargetedMessage and IQueryTargetedMessage interfaces.

--- Messages/IChannelTargetedMessage.cs
+++ Messages/IChannelTargetedMessage.cs
@@ -1,16 +1,12 @@
-using System;
-
 namespace Supay.Irc.Messages {
 
   /// <summary>
-  /// An interface implemented by messages which are, or can be, within the context of a channel.
-  /// </summary>
+  ///   An interface implemented by messages which are, or can be, within the context of a channel. </summary>
   public interface IChannelTargetedMessage {
 
     /// <summary>
-    /// Determines if the the current message is targeted at the given channel.
-    /// </summary>
-    bool IsTargetedAtChannel(String channelName);
-  }
+    ///   Determines if the the current message is targeted at the given channel. </summary>
+    bool IsTargetedAtChannel(string channelName);
 
-}
+  } //interface IChannelTargetedMessage
+} //namespace Supay.Irc.Messages
