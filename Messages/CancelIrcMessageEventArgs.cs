77c8c7a2f43454fab8aed312e50431e1d8d32d8b
_aLfa_ <alfa.project@gmail.com>
2010-03-01 15:37:50 +0000

    Made CancelIrcMessageEventArgs class inherit from IrcMessageEventArgs class.

--- Messages/CancelIrcMessageEventArgs.cs
+++ Messages/CancelIrcMessageEventArgs.cs
@@ -3,33 +3,23 @@ using System;
 namespace Supay.Irc.Messages {
 
   /// <summary>
-  /// The information for a handler of an IrcMessage event which can be canceled.
-  /// </summary>
+  ///   The information for a handler of an IrcMessage event which can be canceled. </summary>
   [Serializable]
-  public class CancelIrcMessageEventArgs<T> : EventArgs where T : IrcMessage {
+  public class CancelIrcMessageEventArgs<T> : IrcMessageEventArgs<T> where T : IrcMessage {
+
     /// <summary>
-    /// Initializes a new instance of the <see cref="IrcMessageEventArgs&lt;T&gt;"/> class with the given <see cref="IrcMessage"/>.
-    /// </summary>
-    public CancelIrcMessageEventArgs(T msg) {
-      this.Message = msg;
+    ///   Initializes a new instance of the <see cref="IrcMessageEventArgs&lt;T&gt;"/>
+    ///   class with the given <see cref="IrcMessage"/>. </summary>
+    public CancelIrcMessageEventArgs(T msg)
+      : base(msg) {
     }
 
     /// <summary>
-    /// Gets or sets the Message for the event.
-    /// </summary>
-    public T Message {
-      get;
-      set;
-    }
-
-    /// <summary>
-    /// Gets or sets a value indicating whether the event should be canceled.
-    /// </summary>
+    ///   Gets or sets a value indicating whether the event should be canceled. </summary>
     public bool Cancel {
       get;
       set;
     }
 
-  }
-
-}
+  } //class CancelIrcMessageEventArgs<T>
+} //namespace Supay.Irc.Messages
