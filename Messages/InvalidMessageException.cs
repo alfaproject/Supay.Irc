7f061f3c24983260aac343680ee0a1ac231288db
_aLfa_ <alfa.project@gmail.com>
2010-03-01 15:58:57 +0000

    Cleaned up InvalidMessageException class.

--- Messages/InvalidMessageException.cs
+++ Messages/InvalidMessageException.cs
@@ -1,116 +1,76 @@
 using System;
-using System.Globalization;
-using System.Runtime.Serialization;
-using System.Security.Permissions;
 
 namespace Supay.Irc.Messages {
 
   /// <summary>
-  /// Exception thrown when a message parsed from a string is invalid.
-  /// </summary>
+  ///   Exception thrown when a message parsed from a string is invalid. </summary>
   [Serializable]
-  public class InvalidMessageException : Exception, ISerializable {
+  public class InvalidMessageException : Exception {
+
+    #region Constructors
 
     /// <summary>
-    /// Initializes a new instance of the InvalidMessageException class.
-    /// </summary>
+    ///   Initializes a new instance of the InvalidMessageException class. </summary>
     public InvalidMessageException()
-      :
-      base() {
+      : base() {
     }
 
     /// <summary>
-    /// Intializes a new instance of the InvalidMessageException class with the given message.
-    /// </summary>
-    /// <param name="message"></param>
-    public InvalidMessageException(String message)
-      :
-      base(message) {
+    ///   Initializes a new instance of the InvalidMessageException class with the given message. </summary>
+    public InvalidMessageException(string message)
+      : base(message) {
     }
 
     /// <summary>
-    /// Intializes a new instance of the InvalidMessageException class with the given message and inner exception.
-    /// </summary>
-    public InvalidMessageException(String message, Exception innerException)
-      :
-      base(message, innerException) {
+    ///   Initializes a new instance of the InvalidMessageException class with the given message and inner exception. </summary>
+    public InvalidMessageException(string message, Exception innerException)
+      : base(message, innerException) {
     }
 
     /// <summary>
-    /// Initializes a new instance of the InvalidMessageException class with the given Message and RecivedMessage.
-    /// </summary>
-    /// <param name="message">Message explaining the exception.</param>
-    /// <param name="receivedMessage">The Message received which was invalid.</param>
-    public InvalidMessageException(String message, String receivedMessage)
-      :
-      base(message) {
-      this.receivedMessage = receivedMessage;
+    ///   Initializes a new instance of the InvalidMessageException class with the given Message and ReceivedMessage. </summary>
+    /// <param name="message">
+    ///   Message explaining the exception. </param>
+    /// <param name="receivedMessage">
+    ///   The Message received which was invalid. </param>
+    public InvalidMessageException(string message, string receivedMessage)
+      : this(message) {
+      this.ReceivedMessage = receivedMessage;
     }
 
     /// <summary>
-    /// Initializes a new instance of the InvalidMessageException class with the given Message and RecivedMessage.
-    /// </summary>
-    /// <param name="message">Message explaining the exception.</param>
-    /// <param name="receivedMessage">The Message received which was invalid.</param>
-    /// <param name="innerException">The exception that exists as the child exception to this one.</param>
-    public InvalidMessageException(String message, String receivedMessage, Exception innerException)
-      :
-      base(message, innerException) {
-      this.receivedMessage = receivedMessage;
+    ///   Initializes a new instance of the InvalidMessageException class with the given Message and RecivedMessage. </summary>
+    /// <param name="message">
+    ///   Message explaining the exception. </param>
+    /// <param name="receivedMessage">
+    ///   The Message received which was invalid. </param>
+    /// <param name="innerException">
+    ///   The exception that exists as the child exception to this one. </param>
+    public InvalidMessageException(string message, string receivedMessage, Exception innerException)
+      : this(message, innerException) {
+      this.ReceivedMessage = receivedMessage;
     }
 
-    #region ISerializable
-    /// <summary>
-    /// deserialization constructor
-    /// </summary>
-    protected InvalidMessageException(SerializationInfo info, StreamingContext context)
-      :
-      base(info, context) {
-      if (info == null) {
-        return;
-      }
-      receivedMessage = info.GetString("ReceivedMessage");
-    }
-
-
-    /// <summary>
-    /// Implements <see cref="ISerializable.GetObjectData"/>.
-    /// </summary>
-    [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.SerializationFormatter)]
-    [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
-    public override void GetObjectData(SerializationInfo info, StreamingContext context) {
-      base.GetObjectData(info, context);
-      if (info != null) {
-        info.AddValue("ReceivedMessage", this.receivedMessage);
-      }
-    }
     #endregion
-
 
     #region Properties
 
     /// <summary>
-    /// Gets the message string that caused the exception.
-    /// </summary>
-    public virtual String ReceivedMessage {
+    ///   Gets the message string that caused the exception. </summary>
+    public string ReceivedMessage {
+      get;
+      private set;
+    }
+
+    /// <summary>
+    ///   Gets the string content of the invalid message. </summary>
+    public override string Message {
       get {
-        return receivedMessage;
+        return base.Message + Environment.NewLine + "ReceivedMessage: " + this.ReceivedMessage;
       }
     }
-    private String receivedMessage = "";
 
     #endregion
 
-    /// <summary>
-    /// Gets the string content of the invalid message.
-    /// </summary>
-    public override string Message {
-      get {
-        string s = String.Format(CultureInfo.InvariantCulture, "ReceivedMessage: {0}", receivedMessage);
-        return base.Message + Environment.NewLine + s;
-      }
-    }
-
-  }
-
-}
+  } //class InvalidMessageException
+} //namespace Supay.Irc.Messages
