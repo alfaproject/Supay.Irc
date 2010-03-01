7082e35bd7006a0373edd276ca8383fe9a1de5ca
_aLfa_ <alfa.project@gmail.com>
2010-03-01 20:06:04 +0000

    Fixed singleton pattern on MessageParserService and cleaned up code.

--- Messages/MessageParserService.cs
+++ Messages/MessageParserService.cs
@@ -5,75 +5,90 @@ using System.Globalization;
 namespace Supay.Irc.Messages {
 
   /// <summary>
-  /// Provides clients with a correct <see cref="IrcMessage"/> for a given raw message string
-  /// </summary>
+  ///   Provides clients with a correct <see cref="IrcMessage"/> for a given raw message string. </summary>
   public sealed class MessageParserService {
 
+    private static readonly MessageParserService _service = new MessageParserService();
+
+    private const int MinMessageLength = 1;
+    private const int MaxMessageLength = 512;
+
+    private PrioritizedMessageList _numerics;
+    private PrioritizedMessageList _commands;
+    private PrioritizedMessageList _ctcps;
+    private PrioritizedMessageList _customs;
+
     /// <summary>
-    /// Initializes a new instance of the <see cref="MessageParserService"/> class.
-    /// </summary>
+    ///   Explicit static constructor to tell C# compiler not to mark type as 'beforefieldinit'. </summary>
+    static MessageParserService() {
+    }
+
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="MessageParserService"/> class. </summary>
     /// <remarks>
-    /// This is private because this class is a Singleton.
-    /// Use the <see cref="Service"/> to get the only instance of this class.
-    /// </remarks>
+    ///   This is private because this class is a Singleton.
+    ///   Use the <see cref="Service"/> to get the only instance of this class. </remarks>
     private MessageParserService() {
+      _numerics = new PrioritizedMessageList();
+      _commands = new PrioritizedMessageList();
+      _ctcps = new PrioritizedMessageList();
+
       foreach (Type type in this.GetType().Assembly.GetTypes()) {
         if (type.IsSubclassOf(typeof(IrcMessage))) {
           if (type.IsSubclassOf(typeof(CommandMessage))) {
             if (!type.IsAbstract) {
-              commands.AddLast((IrcMessage)Activator.CreateInstance(type));
+              _commands.AddLast((IrcMessage)Activator.CreateInstance(type));
             }
           } else if (type.IsSubclassOf(typeof(NumericMessage))) {
             if (!type.IsAbstract && type != typeof(GenericNumericMessage) && type != typeof(GenericErrorMessage)) {
-              numerics.AddLast((IrcMessage)Activator.CreateInstance(type));
+              _numerics.AddLast((IrcMessage)Activator.CreateInstance(type));
             }
           } else if (type.IsSubclassOf(typeof(CtcpMessage))) {
             if (!type.IsAbstract && type != typeof(GenericCtcpRequestMessage) && type != typeof(GenericCtcpReplyMessage)) {
-              ctcps.AddLast((IrcMessage)Activator.CreateInstance(type));
+              _ctcps.AddLast((IrcMessage)Activator.CreateInstance(type));
             }
           }
         }
       }
     }
 
-
     /// <summary>
-    /// Provides access to clients to lone instance of the <see cref="MessageParserService"/>.
-    /// </summary>
-    /// <returns>The Singleton-patterned service.</returns>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2104:DoNotDeclareReadOnlyMutableReferenceTypes")]
-    public static readonly MessageParserService Service = new MessageParserService();
-
-
-    /// <summary>
-    /// Adds a custom message to consider for parsing raw messages recevied from the server.
-    /// </summary>
-    /// <param name="msg"></param>
-    public void AddCustomMessage(IrcMessage msg) {
-      if (this.customs == null) {
-        this.customs = new PrioritizedMessageList();
+    ///   Provides access to clients to lone instance of the <see cref="MessageParserService"/>. </summary>
+    /// <returns>
+    ///   The Singleton-patterned service. </returns>
+    public static MessageParserService Service {
+      get {
+        return _service;
       }
-      customs.AddLast(msg);
     }
 
+    /// <summary>
+    ///   Adds a custom message to consider for parsing raw messages received from the server. </summary>
+    public void AddCustomMessage(IrcMessage msg) {
+      if (_customs == null) {
+        _customs = new PrioritizedMessageList();
+      }
+      _customs.AddLast(msg);
+    }
 
     /// <summary>
-    /// Parses the given string into an <see cref="IrcMessage"/>.
-    /// </summary>
-    /// <param name="unparsedMessage">The string to parse.</param>
-    /// <returns>An <see cref="IrcMessage"/> which represents the given string.</returns>
-    public IrcMessage Parse(String unparsedMessage) {
+    ///   Parses the given string into an <see cref="IrcMessage"/>. </summary>
+    /// <param name="unparsedMessage">
+    ///   The string to parse. </param>
+    /// <returns>
+    ///   An <see cref="IrcMessage"/> which represents the given string. </returns>
+    public IrcMessage Parse(string unparsedMessage) {
       if (unparsedMessage == null) {
-        unparsedMessage = "";
+        unparsedMessage = string.Empty;
       }
       if (unparsedMessage.Length < MinMessageLength || MaxMessageLength < unparsedMessage.Length) {
-        InvalidMessageException e = new InvalidMessageException(String.Format(CultureInfo.InvariantCulture, Properties.Resources.MessageEmptyOrTooLong, unparsedMessage.Length), unparsedMessage);
-        throw (e);
+        string errorMessage = string.Format(CultureInfo.InvariantCulture, Properties.Resources.MessageEmptyOrTooLong, unparsedMessage.Length);
+        throw new InvalidMessageException(errorMessage, unparsedMessage);
       }
 
       IrcMessage msg = null;
       try {
-        msg = DetermineMessage(unparsedMessage);
+        msg = this.DetermineMessage(unparsedMessage);
         msg.Parse(unparsedMessage);
       } catch (InvalidMessageException) {
         throw;
@@ -84,21 +99,20 @@ namespace Supay.Irc.Messages {
     }
 
     /// <summary>
-    /// Determines and instantiates the correct subclass of <see cref="IrcMessage"/> for the given given string.
-    /// </summary>
-    private IrcMessage DetermineMessage(String unparsedMessage) {
+    ///   Determines and instantiates the correct subclass of <see cref="IrcMessage"/> for the given string. </summary>
+    private IrcMessage DetermineMessage(string unparsedMessage) {
       IrcMessage msg = null;
 
-      if (this.customs != null) {
-        msg = GetMessage(unparsedMessage, this.customs);
+      if (_customs != null) {
+        msg = GetMessage(unparsedMessage, _customs);
         if (msg != null) {
           return msg;
         }
       }
 
-      String command = MessageUtil.GetCommand(unparsedMessage);
+      string command = MessageUtil.GetCommand(unparsedMessage);
       if (Char.IsDigit(command[0])) {
-        msg = GetMessage(unparsedMessage, numerics);
+        msg = GetMessage(unparsedMessage, _numerics);
         if (msg == null) {
           int numeric = Convert.ToInt32(command, CultureInfo.InvariantCulture);
           if (NumericMessage.IsError(numeric)) {
@@ -109,7 +123,7 @@ namespace Supay.Irc.Messages {
         }
       } else {
         if (CtcpUtil.IsCtcpMessage(unparsedMessage)) {
-          msg = GetMessage(unparsedMessage, ctcps);
+          msg = GetMessage(unparsedMessage, _ctcps);
           if (msg == null) {
             if (CtcpUtil.IsRequestMessage(unparsedMessage)) {
               msg = new GenericCtcpRequestMessage();
@@ -118,7 +132,7 @@ namespace Supay.Irc.Messages {
             }
           }
         } else {
-          msg = GetMessage(unparsedMessage, commands);
+          msg = GetMessage(unparsedMessage, _commands);
           if (msg == null) {
             msg = new GenericMessage();
           }
@@ -127,7 +141,7 @@ namespace Supay.Irc.Messages {
       return msg;
     }
 
-    private static IrcMessage GetMessage(String unparsedMessage, PrioritizedMessageList potentialHandlers) {
+    private static IrcMessage GetMessage(string unparsedMessage, PrioritizedMessageList potentialHandlers) {
       IrcMessage handler = null;
       LinkedListNode<IrcMessage> nodeToPrioritize = null;
 
@@ -151,7 +165,6 @@ namespace Supay.Irc.Messages {
         while (node != null && node.Next != potentialHandlers.First);
       }
 
-
       if (nodeToPrioritize != null) {
         potentialHandlers.Prioritize(nodeToPrioritize);
       }
@@ -159,14 +172,5 @@ namespace Supay.Irc.Messages {
       return handler;
     }
 
-    private const int MinMessageLength = 1;
-    private const int MaxMessageLength = 512;
-
-    private PrioritizedMessageList numerics = new PrioritizedMessageList();
-    private PrioritizedMessageList commands = new PrioritizedMessageList();
-    private PrioritizedMessageList ctcps = new PrioritizedMessageList();
-    private PrioritizedMessageList customs = null;
-
-  }
-
-}
+  } //class MessageParserService
+} //namespace Supay.Irc.Messages
