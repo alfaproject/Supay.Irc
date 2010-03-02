b5703d2af48e707dfa09d308d009f3cab40c1fa1
_aLfa_ <alfa.project@gmail.com>
2010-03-02 16:08:49 +0000

    Cleaned up Ident class.

--- Network/Ident.cs
+++ Network/Ident.cs
@@ -9,106 +9,104 @@ using System.Threading;
 namespace Supay.Irc.Network {
 
   /// <summary>
-  /// An Ident daemon which is still used by some
-  /// IRC networks for authentication.
-  /// </summary>
-  [System.ComponentModel.DesignerCategory("Code")]
+  ///   An Ident daemon which is still used by some
+  ///   IRC networks for authentication. </summary>
+  [DesignerCategory("Code")]
   public sealed class Ident : Component {
 
-    /// <summary>
-    /// The singleton Ident service.
-    /// </summary>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2104:DoNotDeclareReadOnlyMutableReferenceTypes")]
-    public static readonly Ident Service = new Ident();
+    private static readonly Ident _instance = new Ident();
+
+    private const string REPLY = " : USERID : UNIX : ";
+    private const int PORT = 113;
+
+    private readonly object _syncLock = new object();
+    private TcpListener _listener;
+    private Thread _socketThread;
+    private bool _stopAfter;
+
     private Ident() {
+      User = new User();
+      Status = ConnectionStatus.Disconnected;
+      _stopAfter = true;
     }
 
-
-
     /// <summary>
-    /// Gets or sets the <see cref="Supay.Irc.User"/> to respond to an ident request with.
-    /// </summary>
-    public Supay.Irc.User User {
+    ///   The singleton Ident service. </summary>
+    public static Ident Service {
       get {
-        if (user == null) {
-          user = new User();
-        }
-        return user;
-      }
-      set {
-        user = value;
+        return _instance;
       }
     }
 
     /// <summary>
-    /// Gets the status of the Ident service.
-    /// </summary>
+    ///   Gets or sets the <see cref="Supay.Irc.User"/> to respond to an ident request with. </summary>
+    public User User {
+      get;
+      set;
+    }
+
+    /// <summary>
+    ///   Gets the status of the Ident service. </summary>
     public ConnectionStatus Status {
-      get {
-        return status;
-      }
+      get;
+      private set;
     }
 
-
     /// <summary>
-    /// Starts the Ident server.
-    /// </summary>
+    ///   Starts the Ident server. </summary>
     public void Start() {
-      this.Start(false);
+      Start(false);
     }
 
     /// <summary>
-    /// Starts the Ident server.
-    /// </summary>
-    /// <param name="stopAfterFirstAnswer">If true, Ident will stop immediately after answering. If false, will continue until <see cref="Ident.Stop"/> is called.</param>
+    ///   Starts the Ident server. </summary>
+    /// <param name="stopAfterFirstAnswer">
+    ///   If true, Ident will stop immediately after answering.
+    ///   If false, will continue until <see cref="Ident.Stop"/> is called. </param>
     public void Start(bool stopAfterFirstAnswer) {
-      lock (lockObject) {
-        if (this.status != ConnectionStatus.Disconnected) {
+      lock (_syncLock) {
+        if (Status != ConnectionStatus.Disconnected) {
           System.Diagnostics.Trace.WriteLine("Ident Already Started");
           return;
-        } else {
-          this.stopAfter = stopAfterFirstAnswer;
-          socketThread = new Thread(new ThreadStart(Run));
-          socketThread.Name = "Identd";
-          socketThread.IsBackground = true;
-          socketThread.Start();
         }
+
+        _stopAfter = stopAfterFirstAnswer;
+        _socketThread = new Thread(Run) {
+          Name = "Identd",
+          IsBackground = true
+        };
+        _socketThread.Start();
       }
     }
-
-    private object lockObject = new object();
 
     /// <summary>
-    /// Stops the Ident server.
-    /// </summary>
+    ///   Stops the Ident server. </summary>
     public void Stop() {
-      lock (lockObject) {
-        this.status = ConnectionStatus.Disconnected;
-        if (this.listener != null) {
-          listener.Stop();
+      lock (_syncLock) {
+        Status = ConnectionStatus.Disconnected;
+        if (_listener != null) {
+          _listener.Stop();
         }
       }
     }
 
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1308:NormalizeStringsToUppercase")]
     private void Run() {
-      this.status = ConnectionStatus.Connecting;
+      Status = ConnectionStatus.Connecting;
 
       try {
-        listener = new TcpListener(IPAddress.Any, port);
-        listener.Start();
+        _listener = new TcpListener(IPAddress.Any, PORT);
+        _listener.Start();
       } catch (Exception ex) {
-        System.Diagnostics.Trace.WriteLine("Error Opening Ident Listener On Port " + port.ToString(CultureInfo.InvariantCulture) + ", " + ex.ToString(), "Ident");
-        this.status = ConnectionStatus.Disconnected;
+        System.Diagnostics.Trace.WriteLine("Error Opening Ident Listener On Port " + PORT.ToString(CultureInfo.InvariantCulture) + ", " + ex.ToString(), "Ident");
+        Status = ConnectionStatus.Disconnected;
         throw;
       }
 
       try {
-        while (this.status != ConnectionStatus.Disconnected) {
+        while (Status != ConnectionStatus.Disconnected) {
           try {
-            TcpClient client = listener.AcceptTcpClient();
-            this.status = ConnectionStatus.Connected;
-
+            TcpClient client = _listener.AcceptTcpClient();
+            Status = ConnectionStatus.Connected;
 
             //Read query
             StreamReader reader = new StreamReader(client.GetStream());
@@ -116,23 +114,23 @@ namespace Supay.Irc.Network {
 
             //Send back reply
             StreamWriter writer = new StreamWriter(client.GetStream());
-            String identName = this.User.Username;
+            string identName = User.Username;
             if (identName.Length == 0) {
-              if (this.User.Nickname.Length != 0) {
-                identName = this.User.Nickname;
+              if (User.Nickname.Length != 0) {
+                identName = User.Nickname;
               } else {
                 identName = "supay";
               }
             }
-            String identReply = identRequest.Trim() + this.reply + identName.ToLower(CultureInfo.InvariantCulture);
+            string identReply = identRequest.Trim() + REPLY + identName.ToLower(CultureInfo.InvariantCulture);
             writer.WriteLine(identReply);
             writer.Flush();
 
             //Close connection with client
             client.Close();
 
-            if (stopAfter) {
-              this.status = ConnectionStatus.Disconnected;
+            if (_stopAfter) {
+              Status = ConnectionStatus.Disconnected;
             }
           } catch (IOException ex) {
             System.Diagnostics.Trace.WriteLine("Error Processing Ident Request: " + ex.Message, "Ident");
@@ -150,38 +148,10 @@ namespace Supay.Irc.Network {
         //throw( ex );
       }
 
-      if (listener != null) {
-        listener.Stop();
+      if (_listener != null) {
+        _listener.Stop();
       }
     }
 
-    /// <summary>
-    /// Releases the resources used by <see cref="Ident"/>
-    /// </summary>
-    protected override void Dispose(bool disposing) {
-      try {
-        if (disposing) {
-          if (listener != null) {
-            ((IDisposable)listener).Dispose();
-          }
-          if (socketThread != null) {
-            socketThread.Abort();
-          }
-        }
-      } finally {
-        base.Dispose(disposing);
-      }
-    }
-
-
-    private User user;
-    private TcpListener listener;
-    private Thread socketThread;
-    private string reply = " : USERID : UNIX : ";
-    private int port = 113;
-    private ConnectionStatus status = ConnectionStatus.Disconnected;
-    private bool stopAfter = true;
-
-  }
-
-}
+  } //class Ident
+} //namespace Supay.Irc.Network
