d205c4d46a942ebee93f05c05c70bb283c957e73
_aLfa_ <alfa.project@gmail.com>
2010-03-02 19:16:35 +0000

    Made DccUtil class static and cleaned up comments.

--- Dcc/DccUtil.cs
+++ Dcc/DccUtil.cs
@@ -1,55 +1,42 @@
-using System;
 using System.Collections.Specialized;
+using Supay.Irc.Messages;
 
 namespace Supay.Irc.Dcc {
 
   /// <summary>
-  /// Holds a few utilities for dcc message parsing.
-  /// </summary>
-  public sealed class DccUtil {
+  ///   Holds a few utilities for DCC message parsing. </summary>
+  public static class DccUtil {
 
     /// <summary>
-    /// Do not use under penalty of death
-    /// </summary>
-    private DccUtil() {
-    }
-
-    /// <summary>
-    /// Gets the Dcc Command of the message, such as CHAT or SEND.
-    /// </summary>
-    public static String GetCommand(String rawMessage) {
+    ///   Gets the DCC Command of the message, such as CHAT or SEND. </summary>
+    public static string GetCommand(string rawMessage) {
       return GetParameters(rawMessage)[0];
     }
 
     /// <summary>
-    /// Gets the Dcc Argument of the message, such as the filename of a SEND.
-    /// </summary>
-    public static String GetArgument(String rawMessage) {
+    ///   Gets the DCC Argument of the message, such as the filename of a SEND. </summary>
+    public static string GetArgument(string rawMessage) {
       return GetParameters(rawMessage)[1];
     }
 
     /// <summary>
-    /// Gets the address of the connection instantiator in 64-bits format as a String.
-    /// </summary>
-    public static String GetAddress(String rawMessage) {
+    ///   Gets the address of the connection instantiator in 64-bits format as a String. </summary>
+    public static string GetAddress(string rawMessage) {
       return GetParameters(rawMessage)[2];
     }
 
     /// <summary>
-    /// Gets the port of the connection instantiator.
-    /// </summary>
-    public static String GetPort(String rawMessage) {
+    ///   Gets the port of the connection instantiator. </summary>
+    public static string GetPort(string rawMessage) {
       return GetParameters(rawMessage)[3];
     }
 
     /// <summary>
-    /// Gets the inner parameters of a dcc data area.
-    /// </summary>
-    public static StringCollection GetParameters(String rawMessage) {
-      String extendedData = Supay.Irc.Messages.CtcpUtil.GetExtendedData(rawMessage);
-      return Supay.Irc.Messages.MessageUtil.Tokenize(extendedData, 0);
+    ///   Gets the inner parameters of a DCC data area. </summary>
+    public static StringCollection GetParameters(string rawMessage) {
+      string extendedData = CtcpUtil.GetExtendedData(rawMessage);
+      return MessageUtil.Tokenize(extendedData, 0);
     }
 
-  }
-
-}
+  } //class DccUtil
+} //namespace Supay.Irc.Dcc
