e4b60de49c71939d15bbd8a08094f2947f721183
_aLfa_ <alfa.project@gmail.com>
2010-03-02 16:11:29 +0000

    Cleaned up ConnectionDataEventArgs class.

--- Network/ConnectionDataEvent.cs
+++ Network/ConnectionDataEvent.cs
@@ -3,27 +3,24 @@ using System;
 namespace Supay.Irc.Network {
 
   /// <summary>
-  /// Provides data for connection events that carry a data payload.
-  /// </summary>
+  ///   Provides data for connection events that carry a data payload. </summary>
   public class ConnectionDataEventArgs : EventArgs {
+
     /// <summary>
-    /// Initializes a new instance of the ConnectionDataEventArgs class with the given data.
-    /// </summary>
-    /// <param name="data">The <see cref="ConnectionDataEventArgs.Data"/> <see cref="String"/> received by the <see cref="ClientConnection"/>.</param>
-    public ConnectionDataEventArgs(String data) {
-      this.data = data;
+    ///   Initializes a new instance of the ConnectionDataEventArgs class with the given data. </summary>
+    /// <param name="data">
+    ///   The <see cref="ConnectionDataEventArgs.Data"/> <see cref="String"/>
+    ///   received by the <see cref="ClientConnection"/>. </param>
+    public ConnectionDataEventArgs(string data) {
+      Data = data;
     }
 
     /// <summary>
-    /// Gets the <see cref="ConnectionDataEventArgs.Data"/> received by the the <see cref="ClientConnection"/>.
-    /// </summary>
-    public String Data {
-      get {
-        return data;
-      }
+    ///   Gets the <see cref="ConnectionDataEventArgs.Data"/> received by the the <see cref="ClientConnection"/>. </summary>
+    public string Data {
+      get;
+      private set;
     }
 
-    private String data;
-  }
-
-}
+  } //class ConnectionDataEventArgs
+} //namespace Supay.Irc.Network
