e1c8ee152180d6a3c6c51dafe713c3d8512e0676
_aLfa_ <alfa.project@gmail.com>
2010-03-03 02:40:45 +0000

    Cleaned up DccResumeRequestMessage class.

--- Dcc/DccResumeRequestMessage.cs
+++ Dcc/DccResumeRequestMessage.cs
@@ -6,116 +6,86 @@ using Supay.Irc.Dcc;
 namespace Supay.Irc.Messages {
 
   /// <summary>
-  /// This message is a request to resume sending a file previously, but not completely sent to the requestor.
-  /// </summary>
+  /// This message is a request to resume sending a file previously, but not completely sent to the
+  /// requester. </summary>
   [Serializable]
   public class DccResumeRequestMessage : CtcpRequestMessage {
 
     /// <summary>
-    /// Creates a new instance of the <see cref="DccResumeRequestMessage"/> class.
-    /// </summary>
-    public DccResumeRequestMessage()
-      : base() {
-      this.InternalCommand = "DCC";
+    ///   Creates a new instance of the <see cref="DccResumeRequestMessage"/> class. </summary>
+    public DccResumeRequestMessage() {
+      Position = -1;
+      Port = -1;
+      FileName = string.Empty;
+      InternalCommand = "DCC";
     }
 
     /// <summary>
-    /// Gets the data payload of the Ctcp request.
-    /// </summary>
-    protected override String ExtendedData {
+    ///   Gets the data payload of the CTCP request. </summary>
+    protected override string ExtendedData {
       get {
-        return MessageUtil.ParametersToString(false, this.DccCommand, this.FileName, this.Port.ToString(CultureInfo.InvariantCulture), this.Position.ToString(CultureInfo.InvariantCulture));
+        return MessageUtil.ParametersToString(false, DccCommand, FileName, Port.ToString(CultureInfo.InvariantCulture), Position.ToString(CultureInfo.InvariantCulture));
       }
     }
 
     /// <summary>
-    /// Gets the dcc sub-command.
-    /// </summary>
-    protected virtual String DccCommand {
+    ///   Gets the DCC sub-command. </summary>
+    protected string DccCommand {
       get {
         return "RESUME";
       }
     }
 
     /// <summary>
-    /// Gets or sets the name of the file being sent.
-    /// </summary>
-    public virtual String FileName {
-      get {
-        return fileName;
-      }
-      set {
-        fileName = value;
-      }
+    ///   Gets or sets the name of the file being sent. </summary>
+    public string FileName {
+      get;
+      set;
     }
 
     /// <summary>
-    /// Gets or sets the port the connection should be on.
-    /// </summary>
-    public virtual int Port {
-      get {
-        return port;
-      }
-      set {
-        port = value;
-      }
+    ///   Gets or sets the port the connection should be on. </summary>
+    public int Port {
+      get;
+      set;
     }
 
     /// <summary>
-    /// Gets or sets the position in the file at which to resume sending.
-    /// </summary>
-    public virtual int Position {
-      get {
-        return position;
-      }
-      set {
-        position = value;
-      }
+    ///   Gets or sets the position in the file at which to resume sending. </summary>
+    public int Position {
+      get;
+      set;
     }
 
     /// <summary>
     /// Determines if the message can be parsed by this type.
     /// </summary>
-    public override bool CanParse(String unparsedMessage) {
-      if (!base.CanParse(unparsedMessage)) {
-        return false;
-      }
-
-      return CanParseDccCommand(DccUtil.GetCommand(unparsedMessage));
+    public override bool CanParse(string unparsedMessage) {
+      return base.CanParse(unparsedMessage) && CanParseDccCommand(DccUtil.GetCommand(unparsedMessage));
     }
 
     /// <summary>
-    /// Determines if the message's DCC command is compatible with this message.
-    /// </summary>
-    public virtual bool CanParseDccCommand(String command) {
-      if (command == null) {
-        return false;
-      }
-      return this.DccCommand.EndsWith(command, StringComparison.OrdinalIgnoreCase);
+    ///   Determines if the message's DCC command is compatible with this message. </summary>
+    public bool CanParseDccCommand(string command) {
+      return command != null && DccCommand.EndsWith(command, StringComparison.OrdinalIgnoreCase);
     }
 
     /// <summary>
-    /// Parses the given string to populate this <see cref="IrcMessage"/>.
-    /// </summary>
-    public override void Parse(String unparsedMessage) {
+    ///   Parses the given string to populate this <see cref="IrcMessage"/>. </summary>
+    public override void Parse(string unparsedMessage) {
       base.Parse(unparsedMessage);
-      this.FileName = DccUtil.GetArgument(unparsedMessage);
+      FileName = DccUtil.GetArgument(unparsedMessage);
       StringCollection p = DccUtil.GetParameters(unparsedMessage);
-      this.Port = Convert.ToInt32(p[2], CultureInfo.InvariantCulture);
-      this.Position = Convert.ToInt32(p[3], CultureInfo.InvariantCulture);
+      Port = Convert.ToInt32(p[2], CultureInfo.InvariantCulture);
+      Position = Convert.ToInt32(p[3], CultureInfo.InvariantCulture);
     }
 
     /// <summary>
-    /// Notifies the given <see cref="MessageConduit"/> by raising the appropriate event for the current <see cref="IrcMessage"/> subclass.
-    /// </summary>
-    public override void Notify(Supay.Irc.Messages.MessageConduit conduit) {
+    ///   Notifies the given <see cref="MessageConduit"/> by raising the appropriate event for the
+    ///   current <see cref="IrcMessage"/> subclass. </summary>
+    public override void Notify(MessageConduit conduit) {
       conduit.OnDccResumeRequest(new IrcMessageEventArgs<DccResumeRequestMessage>(this));
     }
 
-    private String fileName = "";
-    private int port = -1;
-    private int position = -1;
-
-  }
-
-}
+  } //class DccResumeRequestMessage
+} //namespace Supay.Irc.Messages
