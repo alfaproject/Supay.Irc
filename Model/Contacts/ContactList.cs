4dcff58ad1039c699a0d1467c011d2fb2e0687e4
_aLfa_ <alfa.project@gmail.com>
2010-03-07 13:53:22 +0000

    Implemented IDisposable on ContactList class.

--- Model/Contacts/ContactList.cs
+++ Model/Contacts/ContactList.cs
@@ -3,94 +3,71 @@ using System;
 namespace Supay.Irc.Contacts {
 
   /// <summary>
-  /// A contact list which tracks the online and offline status of the users within the Users collection property.
-  /// </summary>
+  ///   A contact list which tracks the online and offline status of the users within the Users
+  ///   collection property. </summary>
   /// <remarks>
-  /// The ContactList will use Watch, Monitor, or IsOn, depending on server support. User status changes 
-  /// will be updated via the User.OnlineStatus property.
-  /// </remarks>
+  ///   The ContactList will use Watch, Monitor, or IsOn, depending on server support. User status
+  ///   changes will be updated via the User.OnlineStatus property. </remarks>
   public class ContactList : IDisposable {
 
+    private ContactsTracker _tracker;
+
     /// <summary>
-    /// Gets the collection of users being tracked as a contact list.
-    /// </summary>
+    ///   Gets the collection of users being tracked as a contact list. </summary>
     public UserCollection Users {
       get;
       private set;
     }
 
     /// <summary>
-    /// The client on which the list is tracked.
-    /// </summary>
+    ///   The client on which the list is tracked. </summary>
     public Client Client {
       get;
       private set;
     }
 
     /// <summary>
-    /// Initializes the ContactList on the given client.
-    /// </summary>
+    ///   Initializes the <see cref="ContactList"/> on the given client. </summary>
     /// <remarks>
-    /// This method should not be called until the Client receives the ServerSupport is populated.
-    /// An easy way to make sure is to wait until the Ready event of the Client.
-    /// </remarks>
+    ///   This method should not be called until the Client receives the
+    ///   <see cref="ServerSupport"/> is populated. An easy way to make sure is to wait until the
+    ///   Ready event of the Client. </remarks>
     public void Initialize(Client client) {
       if (client == null) {
         throw new ArgumentNullException("client");
       }
       ServerSupport support = client.ServerSupports;
-      this.Client = client;
-      this.Users = new UserCollection();
+      Client = client;
+      Users = new UserCollection();
 
       if (support.MaxWatches > 0) {
-        this.tracker = new ContactsWatchTracker(this);
+        _tracker = new ContactsWatchTracker(this);
       } else if (support.MaxMonitors > 0) {
-        this.tracker = new ContactsMonitorTracker(this);
+        _tracker = new ContactsMonitorTracker(this);
       } else {
-        this.tracker = new ContactsIsOnTracker(this);
+        _tracker = new ContactsAreOnTracker(this);
       }
 
-      this.tracker.Initialize();
+      _tracker.Initialize();
     }
 
-    private ContactsTracker tracker;
+    #region IDisposable
 
-
-    #region IDisposable Members
-
-    private bool disposed = false;
+    protected virtual void Dispose(bool disposing) {
+      if (disposing && _tracker != null && _tracker is ContactsAreOnTracker) {
+        ((IDisposable)_tracker).Dispose();
+      }
+    }
 
     /// <summary>
-    /// Implements IDisposable.Dispose
-    /// </summary>
+    ///   Performs application-defined tasks associated with freeing, releasing, or resetting
+    ///   unmanaged resources. </summary>
     public void Dispose() {
-      this.Dispose(true);
+      Dispose(true);
       GC.SuppressFinalize(this);
-    }
-
-    /// <summary>
-    /// Implements IDisposable.Dispose
-    /// </summary>
-    private void Dispose(bool disposing) {
-      if (!this.disposed) {
-        if (disposing) {
-          IDisposable disposableTracker = this.tracker as IDisposable;
-          if (disposableTracker != null) {
-            disposableTracker.Dispose();
-          }
-
-        }
-        disposed = true;
-      }
-    }
-
-    /// <exclude />
-    ~ContactList() {
-      Dispose(false);
     }
 
     #endregion
 
-  }
-
-}
+  } //class ContactList
+} //namespace Supay.Irc.Contacts
