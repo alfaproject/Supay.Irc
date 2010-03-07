4dcb6dbb676f072aee55db0d14f0eb72fad0d693
_aLfa_ <alfa.project@gmail.com>
2010-03-07 21:32:07 +0000

    Improved BansEndReplyMessage class.

--- Messages/Replies/BansEndReplyMessage.cs
+++ Messages/Replies/BansEndReplyMessage.cs
@@ -5,22 +5,41 @@ using System.Collections.Specialized;
 namespace Supay.Irc.Messages {
 
   /// <summary>
-  ///   Signifies the end of the reply listing Bans on a channel. </summary>
+  ///   This is sent at the end of a channel ban list, when requested. (with MODE #chan +b.) </summary>
+  /// <remarks>
+  ///   Numeric: 368
+  ///   Name:    RPL_ENDOFBANLIST
+  ///   Syntax:  368 channel :info
+  ///   Example: 368 #howdy :End of Channel Ban List </remarks>
+  /// <seealso cref="BansReplyMessage"/>
   [Serializable]
   public class BansEndReplyMessage : NumericMessage, IChannelTargetedMessage {
 
+    private const string DEFAULT_INFO = "End of Channel Ban List";
+
+    private string _channel;
+    private string _info;
+
     /// <summary>
-    ///   Creates a new instances of the <see cref="BansEndReplyMessage"/> class. </summary>
+    ///   Creates a new instance of the <see cref="BansEndReplyMessage"/> class. </summary>
     public BansEndReplyMessage()
       : base(368) {
-      Channel = string.Empty;
+      _channel = null;
+      _info = DEFAULT_INFO;
     }
 
     /// <summary>
     ///   Gets or sets the channel the ban list refers to. </summary>
-    public string Channel {
-      get;
-      set;
+    public virtual string Channel {
+      get { return _channel; }
+      set { _channel = value; }
+    }
+
+    /// <summary>
+    ///   Gets or sets the info message of this reply. </summary>
+    public virtual string Info {
+      get { return _info; }
+      set { _info = value; }
     }
 
     /// <summary>
@@ -28,7 +47,7 @@ namespace Supay.Irc.Messages {
     protected override Collection<string> GetParameters() {
       Collection<string> parameters = base.GetParameters();
       parameters.Add(Channel);
-      parameters.Add("End of channel ban list");
+      parameters.Add(string.IsNullOrEmpty(Info) ? DEFAULT_INFO : Info);
       return parameters;
     }
 
@@ -38,12 +57,13 @@ namespace Supay.Irc.Messages {
       base.ParseParameters(parameters);
       if (parameters.Count > 1) {
         Channel = parameters[1];
+        Info = (parameters.Count > 2 ? parameters[2] : DEFAULT_INFO);
       }
     }
 
     /// <summary>
-    /// Notifies the given <see cref="MessageConduit"/> by raising the appropriate event for the
-    /// current <see cref="IrcMessage"/> subclass. </summary>
+    ///   Notifies the given <see cref="MessageConduit"/> by raising the appropriate event for the
+    ///   current <see cref="IrcMessage"/> subclass. </summary>
     public override void Notify(MessageConduit conduit) {
       conduit.OnBansEndReply(new IrcMessageEventArgs<BansEndReplyMessage>(this));
     }
