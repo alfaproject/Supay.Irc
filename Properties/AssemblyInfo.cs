3fb2d4ccbc23fdc0c31c18d2a18a51ff2f847ead
_aLfa_ <alfa.project@gmail.com>
2011-11-08 11:47:15 +0000

    Updated copyright information.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Supay IRC Protocol Services")]
 [assembly: AssemblyCompany("_aLfa_")]
 [assembly: AssemblyProduct("Supay IRC")]
-[assembly: AssemblyCopyright("Copyright © _aLfa_ 2006 - 2010")]
+[assembly: AssemblyCopyright("Copyright © _aLfa_ 2006 - 2011")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ab94c7a6-82fb-4da4-bce7-3bfba4d6435e")]
 [assembly: AssemblyVersion("2.1.0.0")]
