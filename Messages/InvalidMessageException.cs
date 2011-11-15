fa14b32bb2f0498f5d3440ca4250c66f3b8812dc
_aLfa_ <alfa.project@gmail.com>
2011-11-15 00:13:13 +0000

    Added a missing serialization constructor to InvalidMessageException.

--- Messages/InvalidMessageException.cs
+++ Messages/InvalidMessageException.cs
@@ -5,7 +5,7 @@ using System.Security.Permissions;
 namespace Supay.Irc.Messages
 {
   /// <summary>
-  ///   Exception thrown when a message parsed from a string is invalid.
+  /// Represents errors that occur during IRC Message parsing.
   /// </summary>
   [Serializable]
   public class InvalidMessageException : Exception
@@ -13,35 +13,38 @@ namespace Supay.Irc.Messages
     #region Constructors
 
     /// <summary>
-    ///   Initializes a new instance of the InvalidMessageException class.
+    /// Initializes a new instance of the <see cref="InvalidMessageException" /> class.
     /// </summary>
     public InvalidMessageException()
     {
     }
 
     /// <summary>
-    ///   Initializes a new instance of the InvalidMessageException class with the given message.
+    /// Initializes a new instance of the <see cref="InvalidMessageException" /> class with a specified error message.
     /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
     public InvalidMessageException(string message)
       : base(message)
     {
     }
 
     /// <summary>
-    ///   Initializes a new instance of the InvalidMessageException class with the given message
-    ///   and inner exception.
+    /// Initializes a new instance of the <see cref="InvalidMessageException" /> class with a specified error message
+    /// and a reference to the inner exception that is the cause of this exception.
     /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
     public InvalidMessageException(string message, Exception innerException)
       : base(message, innerException)
     {
     }
 
     /// <summary>
-    ///   Initializes a new instance of the InvalidMessageException class with the given Message
-    ///   and <see cref="ReceivedMessage" />.
+    /// Initializes a new instance of the <see cref="InvalidMessageException" /> class with a specified error message
+    /// and received IRC Message.
     /// </summary>
-    /// <param name="message">Message explaining the exception.</param>
-    /// <param name="receivedMessage">The Message received which was invalid.</param>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="receivedMessage">The IRC Message received which was invalid.</param>
     public InvalidMessageException(string message, string receivedMessage)
       : this(message)
     {
@@ -49,16 +52,27 @@ namespace Supay.Irc.Messages
     }
 
     /// <summary>
-    ///   Initializes a new instance of the InvalidMessageException class with the given Message
-    ///   and <see cref="ReceivedMessage" />.
+    /// Initializes a new instance of the <see cref="InvalidMessageException" /> class with a specified error message,
+    /// received IRC Message and a reference to the inner exception that is the cause of this exception.
     /// </summary>
-    /// <param name="message">Message explaining the exception.</param>
-    /// <param name="receivedMessage">The Message received which was invalid.</param>
-    /// <param name="innerException">The exception that exists as the child exception to this one.</param>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="receivedMessage">The IRC Message received which was invalid.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
     public InvalidMessageException(string message, string receivedMessage, Exception innerException)
       : this(message, innerException)
     {
       this.ReceivedMessage = receivedMessage;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InvalidMessageException" /> class with serialized data.
+    /// </summary>
+    /// <param name="info">The <see cref="SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
+    /// <param name="context">The <see cref="StreamingContext" /> that contains contextual information about the source or destination.</param>
+    protected InvalidMessageException(SerializationInfo info, StreamingContext context)
+      : base(info, context)
+    {
+      this.ReceivedMessage = info.GetString("ReceivedMessage");
     }
 
     #endregion
@@ -66,7 +80,7 @@ namespace Supay.Irc.Messages
     #region Properties
 
     /// <summary>
-    ///   Gets the message string that caused the exception.
+    /// Gets the IRC Message that caused the exception.
     /// </summary>
     public string ReceivedMessage
     {
@@ -75,7 +89,7 @@ namespace Supay.Irc.Messages
     }
 
     /// <summary>
-    ///   Gets the string content of the invalid message.
+    /// Gets a message that describes the current exception.
     /// </summary>
     public override string Message
     {
@@ -90,11 +104,11 @@ namespace Supay.Irc.Messages
     #region ISerializable
 
     /// <summary>
-    ///   Sets the <see cref="SerializationInfo" /> with information about the exception.
+    /// Sets the <see cref="SerializationInfo" /> with information about the exception.
     /// </summary>
     /// <param name="info">The <see cref="SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
     /// <param name="context">The <see cref="StreamingContext" /> that contains contextual information about the source or destination.</param>
-    /// <exception cref="ArgumentNullException">The <paramref name="info" /> parameter is a null reference (Nothing in Visual Basic).</exception>
+    /// <exception cref="ArgumentNullException">The <paramref name="info" /> parameter is a null reference.</exception>
     [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
     public override void GetObjectData(SerializationInfo info, StreamingContext context)
     {
