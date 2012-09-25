f73a4fd5074a2b69891aca01360d0f74f65f7248
João Dias <alfa.project@gmail.com>
2012-09-25 21:54:08 +0100

    Fixed some documentation

--- Supay.Irc/Network/ConnectionDataEventArgs.cs
+++ Supay.Irc/Network/ConnectionDataEventArgs.cs
@@ -3,21 +3,21 @@ using System;
 namespace Supay.Irc.Network
 {
     /// <summary>
-    ///   Provides data for connection events that carry a data payload.
+    /// Provides data for connection events that carry a data payload.
     /// </summary>
     public class ConnectionDataEventArgs : EventArgs
     {
         /// <summary>
-        ///   Initializes a new instance of the ConnectionDataEventArgs class with the given data.
+        /// Initializes a new instance of the <see cref="ConnectionDataEventArgs"/> class with the given data.
         /// </summary>
-        /// <param name="data">The <see cref="ConnectionDataEventArgs.Data" /> <see cref="string" /> received by the <see cref="ClientConnection" />.</param>
+        /// <param name="data">The <see cref="Data"/> <see cref="string"/> received by the <see cref="ClientConnection"/>.</param>
         public ConnectionDataEventArgs(string data)
         {
             this.Data = data;
         }
 
         /// <summary>
-        ///   Gets the <see cref="ConnectionDataEventArgs.Data" /> received by the the <see cref="ClientConnection" />.
+        /// Gets the <see cref="Data"/> received by the the <see cref="ClientConnection"/>.
         /// </summary>
         public string Data
         {
--- Supay.Irc/Network/ConnectionStatus.cs
+++ Supay.Irc/Network/ConnectionStatus.cs
@@ -1,22 +1,22 @@
 namespace Supay.Irc.Network
 {
     /// <summary>
-    ///   Specifies the status of a <see cref="ClientConnection" />.
+    /// Specifies the status of a <see cref="ClientConnection"/>.
     /// </summary>
     public enum ConnectionStatus
     {
         /// <summary>
-        ///   The connection is broken and not able to transmit.
+        /// The connection is broken and not able to transmit.
         /// </summary>
         Disconnected,
 
         /// <summary>
-        ///   The connection is being made.
+        /// The connection is being made.
         /// </summary>
         Connecting,
 
         /// <summary>
-        ///   The connection is complete and ready to transmit.
+        /// The connection is complete and ready to transmit.
         /// </summary>
         Connected,
     }
